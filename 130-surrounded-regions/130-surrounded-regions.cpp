b21a260fed3d37e04fd80219fa8216d6e2bf7066
adityaajay29 <90138094+adityaajay29@users.noreply.github.com>
2022-04-06 18:18:07 +0530

    Time: 13 ms (74.26%), Space: 10 MB (76.43%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -14,37 +14,39 @@ public:
     }
     
     void solve(vector<vector<char>>& board) {
-    
+
         int m = board.size();
         
-      if(m == 0) return;  
+        if(m == 0) return;  
         
-     int n = board[0].size();
+        int n = board[0].size();
      
-     //Moving over firts and last column   
-     for(int i=0; i<m; i++) {
-         if(board[i][0] == 'O')
-             dfs(board, i, 0, m, n);
-         if(board[i][n-1] == 'O')
-             dfs(board, i, n-1, m, n);
-     }
-        
-        
-     //Moving over first and last row   
-     for(int j=0; j<n; j++) {
-         if(board[0][j] == 'O')
-             dfs(board, 0, j, m, n);
-         if(board[m-1][j] == 'O')
-             dfs(board, m-1, j, m, n);
-     }
-        
-     for(int i=0; i<m; i++)
-         for(int j=0; j<n; j++)
+        //Moving over first and last column   
+        for(int i=0; i<m; i++) {
+             if(board[i][0] == 'O')
+                 dfs(board, i, 0, m, n);
+             if(board[i][n-1] == 'O')
+                 dfs(board, i, n-1, m, n);
+         }
+
+
+         //Moving over first and last row   
+         for(int j=0; j<n; j++) {
+             if(board[0][j] == 'O')
+                 dfs(board, 0, j, m, n);
+             if(board[m-1][j] == 'O')
+                 dfs(board, m-1, j, m, n);
+         }
+
+         for(int i=0; i<m; i++)
          {
-             if(board[i][j] == 'O')
-                 board[i][j] = 'X';
-             if(board[i][j] == '#')
-                 board[i][j] = 'O';
+             for(int j=0; j<n; j++)
+             {
+                 if(board[i][j] == 'O')
+                     board[i][j] = 'X';
+                 if(board[i][j] == '#')
+                     board[i][j] = 'O';
+             }
          }
     }
 };
