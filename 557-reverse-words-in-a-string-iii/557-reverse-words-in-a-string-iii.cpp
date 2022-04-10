7e211067d65c97157c44bfed733780922e46c72c
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-04-10 23:02:58 +0530

    Time: 19 ms (52.33%), Space: 9.7 MB (44.99%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int i=0;
+        int n=s.size();
+        for(int j=0;j<n;j++)
+        {
+//             reversing all the words except for last word
+            if(s[j]==' ')
+            {
+                reverse(s.begin()+i,s.begin()+j);
+                i=j+1;
+            }
+        }
+//         reversing last word
+        reverse(s.begin()+i,s.end());
+        return s;
+    }
+};
