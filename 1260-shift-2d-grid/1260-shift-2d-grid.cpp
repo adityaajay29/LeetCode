911071861ea859390055ab8f0b0a72f55c880a09
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-04-11 11:48:03 +0530

    Time: 98 ms (13.96%), Space: 13.9 MB (87.46%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -3,22 +3,29 @@ public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
         int m=grid.size();
         int n=grid[0].size();
+//         creating a temp grid for making changes
         vector<vector<int> > ans;
+//         temp grid is same as given grid
         ans=grid;
+//         running loop k times
         while(k--)
         {
             for(int i=0;i<m;i++)
             {
                 for(int j=1;j<n;j++)
                 {
+//                     shifting each row 1 column ahead
                     ans[i][j]=grid[i][j-1];
                 }
             }
             for(int i=0;i<m-1;i++)
             {
+//                 2nd operation
                 ans[i+1][0]=grid[i][n-1];
             }
+//             3rd aoperation
             ans[0][0]=grid[m-1][n-1];
+//             since our new grid will be the temp grid, for multiple shifts, updating the given grid
             grid=ans;
         }
         return ans;
