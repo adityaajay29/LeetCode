04b540a63aa660a3f6e05b2d799feb5c02a64549
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-04-13 12:33:12 +0530

    Time: 4 ms (19.27%), Space: 6.6 MB (18.13%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -4,23 +4,23 @@ public:
         vector<vector<int>>v(n,vector<int>(n,0));
         int layer=(n+1)/2;
         int cnt=1;
-        for(int j=0;j<layer;j++)
+        for(int i=0;i<layer;i++)
         {
-            for(int i=j;i<n-j;i++)
-            {
-                v[j][i]=cnt++;
-            }
-            for(int i=j+1;i<(n-j);i++)
-            {
-                v[i][n-j-1]=cnt++;
-            }
-            for(int i=n-j-2;i>=j;i--)
-            {
-                v[n-j-1][i]=cnt++;
-            }
-            for(int i=n-j-2;i>j;i--)
+            for(int j=i;j<n-i;j++)
             {
                 v[i][j]=cnt++;
+            }
+            for(int j=i+1;j<(n-i);j++)
+            {
+                v[j][n-i-1]=cnt++;
+            }
+            for(int j=n-i-2;j>=i;j--)
+            {
+                v[n-i-1][j]=cnt++;
+            }
+            for(int j=n-i-2;j>i;j--)
+            {
+                v[j][i]=cnt++;
             }
         }
         return v;
