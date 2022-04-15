2d128194879a70aa124095d4e69a4db987ef7c21
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-04-15 14:53:28 +0530

    Time: 20 ms (61.23%), Space: 24 MB (7.08%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
@@ -12,17 +12,25 @@
 class Solution {
 public:
     TreeNode* trimBST(TreeNode* root, int low, int high) {
+//         if we reach null, return NULL
         if(!root)
             return NULL;
+//         if the curr val lies in the required range or is low/high
+//         then make the left and right connections from left and right subtreees respectively
         if(root->val>=low && root->val<=high)
         {
+//             making left connection
             root->left=trimBST(root->left,low,high);
+//             making right connection
             root->right=trimBST(root->right,low,high);
-            return root;
         }
+//         if below range, goto right 
         if(root->val<low)
             return trimBST(root->right,low,high);
-        else
+//         else, go to left
+        else if(root->val>high)
             return trimBST(root->left,low,high);
+//             return the root after all connections
+            return root;
     }
 };
