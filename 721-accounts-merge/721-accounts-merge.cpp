7f5e979ca361140f2be0c4ba5aba970eb9c8d4fd
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-04-17 01:02:48 +0530

    Time: 149 ms (66.69%), Space: 50.9 MB (22.55%) - LeetHub

--- 721-accounts-merge/721-accounts-merge.cpp
+++ 721-accounts-merge/721-accounts-merge.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     unordered_map<string,vector<string> >mp;
     unordered_set<string>visited;
+    
     void dfs(string &email,vector<string>&temp)
     {
         visited.insert(email);
@@ -15,6 +16,7 @@ public:
     
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
         vector<vector<string> >ans;
+//             making adjecency list for all the "emails" assosiated with an "account"
         for(vector<string>v:accounts)
         {
             for(int i=2;i<v.size();i++)
@@ -23,13 +25,14 @@ public:
                 mp[v[i-1]].push_back(v[i]);
             }
         }
-        
+//         if email is not already visited, then add it to the specified account
+//         call for next emails in that account 
+//         sort the emails for that account
         for(vector<string>v:accounts)
         {
             if(visited.find(v[1])==visited.end())
             {
                 vector<string>temp;
-                temp.clear();
                 temp.push_back(v[0]);
                 dfs(v[1],temp);
                 sort(temp.begin()+1,temp.end());
