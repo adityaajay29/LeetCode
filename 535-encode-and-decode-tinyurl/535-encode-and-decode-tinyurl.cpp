d100196353b3d07805975f3563cf3c3b69d0653f
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-04-23 22:10:46 +0530

    Time: 4 ms (70.07%), Space: 7.3 MB (16.54%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
-    
-//     creating a map to store short url and its corresponding long url
     unordered_map<string,string>mp;
-//     starting our coding with 
-    int codeVal=0;
-
+    int code=1;
+    string codeWord="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+//     function to generate random code words depending on value of code
+    string append()
+    {
+        string key;
+        int n=code;
+        while(n>0)
+        {
+            key.push_back(codeWord[code%62]);
+            n/=62;
+        }
+        return key;
+    }
+    
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        string code="http://tinyurl.com/"+to_string(codeVal++);
-        mp[code]=longUrl;
-        // cout<<code<<" "<<mp[code];
-        return code;
+        // cout<<append()<<endl;
+        string ans="https://tinyurl.com/"+append();
+//         updating the value of code
+        code++;
+//         making short long code key val pair
+        mp[ans]=longUrl;
+        return ans;
     }
 
     // Decodes a shortened URL to its original URL.
