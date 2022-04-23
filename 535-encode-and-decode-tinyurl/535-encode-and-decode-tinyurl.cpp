8e4d5018bb8c93b85074202110435f1869f0478c
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-04-23 19:56:34 +0530

    Time: 4 ms (70.07%), Space: 7.2 MB (29.61%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     
+//     creating a map to store short url and its corresponding long url
+    unordered_map<string,string>mp;
+//     starting our coding with 
+    int codeVal=0;
+
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        return longUrl ;
+        string code="http://tinyurl.com/"+to_string(codeVal++);
+        mp[code]=longUrl;
+        // cout<<code<<" "<<mp[code];
+        return code;
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return shortUrl;
+        return mp[shortUrl];
     }
 };
 
