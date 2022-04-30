dec96390707f3c2fca8e3aa541ce5a92b4a888a9
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-05-01 01:21:22 +0530

    Time: 5 ms (30.49%), Space: 8.3 MB (52.39%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.cpp
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     
-    void dfs(string x, string y, double &val, map<string,int> &visited, map<string, double> &mp, map<string, vector<string> > &list, bool &flag)
+    void dfs(string x, string y, double &val, unordered_map<string,int> &visited, unordered_map<string, double> &mp, unordered_map<string, vector<string> > &list, bool &flag)
     {
         visited[x]=1;
         for(string child:list[x])
         {
             if(visited[child]!=1)
             {
+//                 multiply with child weight
                 val*=mp[x+"->"+child];
                 if(child==y)
                 {
@@ -18,22 +19,25 @@ public:
                 if(flag==true)
                     return;
                 else 
+//                     if coudn't find the way from x to y, then undo the miultiplication
                     val/=mp[x+"->"+child];
             }
         }
     }
     
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        map<string, double> mp;
-        map<string, vector<string> > list;
+        unordered_map<string, double> mp;
+        unordered_map<string, vector<string> > list;
         vector<double>ans;
         int n=values.size();
         for(int i=0;i<n;i++)
         {
             string u=equations[i][0];
             string v=equations[i][1];
+//             mp to store the weight
             mp[u+"->"+v]=values[i];
             mp[v+"->"+u]=1/values[i];
+//             list to make the adjecency list
             list[u].push_back(v);
             list[v].push_back(u);
         }
@@ -41,11 +45,14 @@ public:
         {
             string x=queries[i][0];
             string y=queries[i][1];
+//             if connection not in the list, then -1
             if(list.find(x)==list.end() || list.find(y)==list.end())
                 ans.push_back(-1);
+//             else if x==y, then result will be 1,
+//             else call dfs to chek for multiplication or devision, whichever satisfyies the relation btw x&y
             else 
             {
-                map<string,int>visited;
+                unordered_map<string,int>visited;
                 double val=1;
                 bool flag=false;
                 if(x==y)
