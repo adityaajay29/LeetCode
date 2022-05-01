6bf48d32e45c7838aadc2254b3005b3d6f598a39
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-05-01 17:36:24 +0530

    Time: 5 ms (18.70%), Space: 6 MB (99.98%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
+        if(s==t)
+            return true;
         int n=s.size();
         int m=t.size();
+//         count the backspaces
         int count=0;
         string a,b;
+//         start from last char
         for(int i=n-1;i>=0;i--)
         {
+//             if s[i] isn't a backspace, check if there were previous backspaces or not
             if(s[i]!='#')
             {
+//                 if there were backspaces, then skip that char and decrease the count 
                 if(count>0)
                     count--;
                 else 
+//                     if there were no backspaces, push that char to string, as it will not be erased
                     a.push_back(s[i]);
             }
+//             if char is a backspace, then increase its count
             else 
             {
                 count++;
             }
         }
+//         check the same for 2nd string
         count=0;
         for(int i=m-1;i>=0;i--)
         {
@@ -34,6 +43,7 @@ public:
                 count++;
             }
         }
+//         return true if both the strings a,b are same at last
         return a==b;
     }
 };
