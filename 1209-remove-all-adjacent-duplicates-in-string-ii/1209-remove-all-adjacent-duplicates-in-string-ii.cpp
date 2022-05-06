68507884dac71b261d33e69a3ad3d6921c3423a4
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-05-06 17:03:06 +0530

    Time: 40 ms (39.37%), Space: 10.4 MB (57.07%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -6,32 +6,29 @@ public:
         st.push({s[0],1});
         for(int i=1;i<n;i++)
         {
-            if(!st.empty())
+            if(st.empty() || st.top().first != s[i])
             {
-                if(st.top().first == s[i])
-                {
-                    st.push({s[i],st.top().second+1});
-                    if(st.top().second > k || st.top().second == k)
-                    {
-                        int count=k;
-                        while(count && !s.empty())
-                        {
-                            st.pop();
-                            count--;
-                        }
-                    }
-                }
-                else 
-                    st.push({s[i],1});
+                st.push({s[i],1});
             }
             else 
-                st.push({s[i],1});
+            {
+                int topN=st.top().second;
+                st.pop();
+                st.push({s[i],topN+1});
+            }
+            if(st.top().second == k)
+                st.pop();
         }
         string ans;
         while(!st.empty())
         {
-            ans.push_back(st.top().first);
+            char topC=st.top().first;
+            int topN=st.top().second;
             st.pop();
+            while(topN--)
+            {
+                ans.push_back(topC);
+            }
         }
         reverse(ans.begin(), ans.end());
         return ans;
