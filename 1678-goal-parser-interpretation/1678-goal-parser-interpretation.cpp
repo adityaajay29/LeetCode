816a1ad3183b8e11e107688c39990f422f69b490
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-05-16 00:18:54 +0530

    Time: 3 ms (36.15%), Space: 6 MB (97.67%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string ans;
+        for(int i=0;i<command.size();i++)
+        {
+            if(command[i] == 'G')
+                ans.push_back('G');
+            else if(command[i] == '(')
+            {
+                if(command[i+1] == ')')
+                {
+                    ans.push_back('o');
+                    i+=1;
+                }
+                else 
+                {
+                    ans.push_back('a');
+                    ans.push_back('l');
+                    i+=3;
+                }
+            }
+        }
+        return ans;
+    }
+};
