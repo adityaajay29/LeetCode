9ffa049ce867dadbc54f3569aa105f378e8cfa0d
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-05-19 18:42:17 +0530

    Time: 83 ms (80.60%), Space: 10.6 MB (79.78%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,47 +1,23 @@
 class Solution {
 public:
+    
     int dp[14][10002];
     
-    int knapsack(vector<int> &coins, int amount, int n)
+    int topDown(vector<int> &coins, int n, int amount)
     {
-        for(int i=0;i<=n;i++)
-        {
-            for(int j=0;j<=amount;j++)
-            {
-                if(i == 0)
-                    dp[i][j] = INT_MAX;
-                if(j == 0)
-                    dp[i][j] = 0;
-            }
-        }
-        
-        dp[0][0] = INT_MAX-1;
-        
-        for(int j=1;j<=amount;j++)
-        {
-            if(j % coins[0] == 0)
-                dp[1][j] = j/coins[0];
-            else
-                dp[1][j] = INT_MAX-1;
-        }
-        
-        for(int i=2;i<=n;i++)
-        {
-            for(int j=1;j<=amount;j++)
-            {
-                if(coins[i-1] <= j)
-                    dp[i][j] = min(1 + dp[i][j-coins[i-1]], dp[i-1][j]);
-                else 
-                    dp[i][j] = dp[i-1][j];
-            }
-        }
-        if(dp[n][amount] == INT_MAX-1)
-            return -1;
-        return dp[n][amount];
+        if(n == 0)
+            return (amount == 0 ? 0 : INT_MAX-1);
+        if(dp[n][amount] != -1)
+            return dp[n][amount];
+        if(coins[n-1] <= amount)
+            return dp[n][amount] = min(1 + topDown(coins, n, amount-coins[n-1]), topDown(coins, n-1, amount));
+        else
+            return dp[n][amount] = topDown(coins, n-1, amount);
     }
     
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        return knapsack(coins, amount, n);
+        memset(dp, -1, sizeof(dp));
+        return (topDown(coins, n, amount) == INT_MAX-1 ? -1 : topDown(coins, n, amount));
     }
 };
