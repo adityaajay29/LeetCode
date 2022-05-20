925c2dfdc6c9453512ef359b3370bdd86cd6683b
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-05-20 09:32:19 +0530

    Time: 3 ms (80.74%), Space: 7.7 MB (73.85%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -8,17 +8,11 @@ public:
         int n=obstacleGrid[0].size();
         for(int j=1;j<n;j++)
         {
-            if(obstacleGrid[0][j] == 0 && obstacleGrid[0][j-1] == 1)
-                obstacleGrid[0][j] = 1;
-            else
-                obstacleGrid[0][j] = 0;
+            obstacleGrid[0][j] = (obstacleGrid[0][j] == 1 ? 0 : obstacleGrid[0][j-1]);
         }
         for(int i=1;i<m;i++)
         {
-            if(obstacleGrid[i][0] == 0 && obstacleGrid[i-1][0] == 1)
-                obstacleGrid[i][0] = 1;
-            else
-                obstacleGrid[i][0] = 0;
+            obstacleGrid[i][0] = (obstacleGrid[i][0] == 1 ? 0 : obstacleGrid[i-1][0]);
         }
         for(int i=1;i<m;i++)
         {
