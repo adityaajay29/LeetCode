b77965d6e9835f75e9b9903b1238e8b83d4eb5fd
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-05-21 09:39:31 +0530

    Time: 140 ms (36.71%), Space: 10.6 MB (84.79%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     
-    int dp[14][10002];
+    int dp[13][10001];
     
-    int topDown(vector<int> &coins, int n, int amount)
+    int helper(vector<int> &coins, int n, int amount)
     {
         if(n == 0)
+        {
             return (amount == 0 ? 0 : INT_MAX-1);
+        }
         if(dp[n][amount] != -1)
             return dp[n][amount];
         if(coins[n-1] <= amount)
-            return dp[n][amount] = min(1 + topDown(coins, n, amount-coins[n-1]), topDown(coins, n-1, amount));
+            return dp[n][amount] = min(1 + helper(coins, n, amount-coins[n-1]), helper(coins, n-1, amount));
         else
-            return dp[n][amount] = topDown(coins, n-1, amount);
+            return dp[n][amount] = helper(coins, n-1, amount);
     }
     
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         memset(dp, -1, sizeof(dp));
-        return (topDown(coins, n, amount) == INT_MAX-1 ? -1 : topDown(coins, n, amount));
+        return (helper(coins, n, amount) == INT_MAX-1 ? -1 : helper(coins, n, amount));
     }
 };
