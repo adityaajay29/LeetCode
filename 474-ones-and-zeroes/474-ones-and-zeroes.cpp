0556c5d5bce8d151b30375ca88e7e0150345cdbc
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-05-23 12:31:58 +0530

    Time: 93 ms (96.82%), Space: 8.7 MB (98.79%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,41 +1,25 @@
 class Solution {
 public:
     
-    int dp[601][101][101];
-    
-    int helper(vector<pair<int, int>> &store, int m, int n, int sz)
-    {
-        if(sz == 0 || (n == 0 && m == 0))
-            return 0;
+    int dp[101][101];
         
-        if(dp[sz][m][n] != -1)
-            return dp[sz][m][n];
-        
-        if(store[sz-1].first > m || store[sz-1].second > n)
-            return dp[sz][m][n] = helper(store, m, n, sz-1);
-        
-        int include = 1 + helper(store, m - store[sz-1].first, n - store[sz-1].second, sz-1);
-        int exclude = helper(store, m, n, sz-1);
-        
-        return dp[sz][m][n] = max(include, exclude);
-    }
-     
     int findMaxForm(vector<string>& strs, int m, int n) {
-        vector<pair<int, int>> store;
         for(string s : strs)
         {
-            int count0 = 0;
-            int count1 = 0;
+            int c0 = 0;
+            int c1 = 0;
             for(char c : s)
             {
-                if(c == '0')
-                    count0++;
-                else 
-                    count1++;
+                (c == '0' ? c0++ : c1++);
             }
-            store.push_back({count0, count1});
+            for(int i=m;i>=c0;i--)
+            {
+                for(int j=n;j>=c1;j--)
+                {
+                    dp[i][j] = max(1 + dp[i-c0][j-c1], dp[i][j]);
+                }
+            }
         }
-        memset(dp, -1, sizeof(dp));
-        return helper(store, m, n, store.size());
+        return dp[m][n];
     }
 };
