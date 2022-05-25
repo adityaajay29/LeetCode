3c7fdc0e690d5f2da66ebf0dcd8d92b43b29f455
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-05-25 18:53:33 +0530

    Time: 3 ms (87.12%), Space: 12.2 MB (37.89%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n=nums.size();
+        int n = nums.size();
         int i;
-//         finding the first number from end such that nums[i]>nums[i-1]
-        for(i=n-1;i>0;i--)
+//         finding the point after which decreasing order starts
+//         as decreasing order will not generate next permutation 
+        for(i=n-1;i>=1;i--)
         {
-            if(nums[i-1]<nums[i])
+            if(nums[i-1] < nums[i])
                 break;
         }
-//         if no smaller found, meaning the array is reverse sorted, so simply reverse it
-        if(i==0)
+//         array is in reverse order
+        if(i == 0)
         {
-            reverse(nums.begin(),nums.end());
+            reverse(nums.begin(), nums.end());
             return;
         }
-//         now, all the elements are sorted in decreasing order (from i), as we already found a smaller ele, nums[i-1]
-//         so, we will find a number just greater than nums[i-1]
         int j;
-        for(j=n-1;j>=i;j--)
+//         finding the ele from last, just greater than the break point ele
+        for(j=n-1;j>i-1;j--)
         {
-            if(nums[j]>nums[i-1])
+            if(nums[j] > nums[i-1])
                 break;
         }
-//         swapped the just larger num with nums[i-1]
-        swap(nums[j],nums[i-1]);
-//         reverse the rest of the array after i-1, as this will give the lexicographically smallest permurtation
-//         since the array after i-1 was reverse sorted
-        reverse(nums.begin()+i,nums.end());
+//         even on swapping, the eles after break point will be in decreasing order
+        swap(nums[j], nums[i-1]);
+//         reverse after break point to get assending order, and hence the next perm
+        reverse(nums.begin() + i, nums.end());
     }
 };
