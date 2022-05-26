3dddd3313b63b410c63f046cfd78a8ebdf3d997e
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-05-26 16:34:39 +0530

    Time: 50 ms (53.27%), Space: 20.1 MB (27.54%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> ans;
+        vector<vector<int>> merged;
+//         sort the intervals
         sort(intervals.begin(), intervals.end());
         vector<int> temp = intervals[0];
-        for(auto v : intervals)
+        for(vector<int> v : intervals)
         {
+//             if they are not overlapping, temp will never overlap again
+//             so push temp to ans, and make v as new temp
             if(v[0] > temp[1])
             {
-                ans.push_back(temp);
+                merged.push_back(temp);
                 temp = v;
             }
             else
+//                 merge temp and v
             {
                 temp[1] = max(temp[1], v[1]);
             }
         }
-        ans.push_back(temp);
-        return ans;
+//         at last put the left temp in ans
+        merged.push_back(temp);
+        return merged;
     }
 };
