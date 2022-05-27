15c1d7f472aa4441788803e6d6510329537ad7ea
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-05-27 15:04:43 +0530

    Time: 20 ms (6.02%), Space: 35.5 MB (13.37%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -16,31 +16,33 @@ public:
     double myPow(double x, int n) {
         if(x == 0)
             return 0;
+        
         if(x == 1)
             return 1;
+        
         if(x == -1)
         {
             return (abs(n)%2 == 0 ? 1 : -1);
         }
+        
         if(n == INT_MAX)
         {
             if(x > 1)
                 return INT_MAX;
-            else if(x < 1 && x > 0)
-                return 0;
-            else if(x < 0)
+            else if(x < 1)
                 return 0;
         }
+        
         if(n == INT_MIN)
         {
-            if(x > 1)
+            if(x > 1 || x < 0)
                 return 0;
             else if(x < 1 && x > 0)
                 return INT_MAX;
-            else if(x < 0)
-                return 0;
         }
+        
         ans = 1;
+        
         if(n < 0)
             return pow(1/x, n*(-1));
         else
