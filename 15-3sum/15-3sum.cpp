563fe282f02f2201e2ac9274d08322b12b853cbe
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-05-28 16:47:22 +0530

    Time: 118 ms (58.24%), Space: 21.3 MB (40.11%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,50 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int> > ans;
-//         sort nums
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-//         corner case 1
-        if(n<3)
-            return {};
-//         corner case 2
-        if(nums[0]>0)
-            return {};
-//         itearting by fixing 1st num and so on
+        int n = nums.size();
+        int target = 0;
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
         for(int i=0;i<n;i++)
         {
-//             escape same adj ele
-            if(i>0 && nums[i]==nums[i-1])
-                continue;
-//             finding 3sum
-            int l=i+1;
-            int h=n-1;
-            while(l<h)
+            int target2 = target - nums[i];
+            int left = i+1;
+            int right = n-1;
+            while(left < right)
             {
-                if(nums[l] + nums[h] + nums[i]==0)
-                {
-                    vector<int>v{nums[l],nums[h],nums[i]};
-                    ans.push_back(v);
-//                     escape all duplicates
-                    while(l<h && nums[l]==nums[l+1])
-                    {
-                        l++;
-                    }
-//                     escape all duplicates
-                    while(l<h && nums[h]==nums[h-1])
-                    {
-                        h--;
-                    }
-//                     to look for next sets off unique eles
-                    l++;
-                    h--;
-                }
-                else if(nums[l]+nums[h]+nums[i]<0)
-                    l++;
+                int twoSum = nums[left] + nums[right];
+                if(twoSum < target2)
+                    left++;
+                else if(twoSum > target2)
+                    right--;
                 else 
-                    h--;
+                {
+                    vector<int> temp{nums[i], nums[left], nums[right]};
+                    ans.push_back(temp);
+                    while(left < right && nums[left] == temp[1])
+                        left++;
+                    while(left < right && nums[right] == temp[2])
+                        right--;
+                }
             }
+            while(i+1 < n && nums[i+1] == nums[i])
+                i++;
         }
         return ans;
     }
