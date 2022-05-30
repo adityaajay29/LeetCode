303c9b9229c13e3bff2b9e5131976dcc425acfaf
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-05-30 19:21:13 +0530

    Time: 80 ms (27.33%), Space: 14.4 MB (98.97%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -13,14 +13,8 @@ public:
         ListNode *curr2 = headB;
         while(curr1 != curr2)
         {
-            if(curr1 != nullptr)
-                curr1 = curr1->next;
-            else
-                curr1 = headB;
-            if(curr2 != nullptr)
-                curr2 = curr2->next;
-            else
-                curr2 = headA;
+            curr1 = (curr1 == nullptr ? headB : curr1->next);
+            curr2 = (curr2 == nullptr ? headA : curr2->next);
         }
         return curr1;
     }
