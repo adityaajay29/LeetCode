ee794289f5e0933eed48ea9ad23fdab9c11a7245
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-06-01 22:21:47 +0530

    Time: 74 ms (93.68%), Space: 21.3 MB (36.73%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
         int n = nums.size();
         int target = 0;
-        vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
+        
         for(int i=0;i<n;i++)
         {
             int target2 = target - nums[i];
-            int left = i+1;
-            int right = n-1;
-            while(left < right)
+            int l = i+1;
+            int h = n-1;
+            while(l < h)
             {
-                int twoSum = nums[left] + nums[right];
-                if(twoSum < target2)
-                    left++;
-                else if(twoSum > target2)
-                    right--;
-                else 
+                int twoSum = nums[l] + nums[h];
+                if(target2 > twoSum)
                 {
-                    vector<int> temp{nums[i], nums[left], nums[right]};
+                    l++;
+                }
+                else if(target2 < twoSum)
+                {
+                    h--;
+                }
+                else
+                {
+                    vector<int> temp{nums[i], nums[l], nums[h]};
                     ans.push_back(temp);
-                    while(left < right && nums[left] == temp[1])
-                        left++;
-                    while(left < right && nums[right] == temp[2])
-                        right--;
+                    l++;
+                    h--;
+                    while(l < h && nums[l] == temp[1])
+                        l++;
+                    while(l < h &&  nums[h] == temp[2])
+                        h--;
                 }
             }
-            while(i+1 < n && nums[i+1] == nums[i])
+            while(i + 1 < n && nums[i] == nums[i+1])
                 i++;
         }
         return ans;
