fef2d7334634fd3745e75842331aae76cc8a7758
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-06-10 12:35:06 +0530

    Time: 15 ms (74.02%), Space: 12 MB (45.74%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,30 +1,5 @@
 class Solution {
 public:
-    int solve(vector<int> &nums, int i, int sum, vector<vector<int>> &dp)
-    {
-        if(i == 0)
-        { 
-            if(nums[0] == 0 && sum == 0)
-                return 2;
-            
-            if(nums[0] == sum || sum == 0)
-                return 1;
-            
-            return 0;
-        }
-        
-        if(dp[i][sum] != -1)
-            return dp[i][sum];
-        
-        int take = 0;
-        int notTake = solve(nums, i - 1, sum, dp);
-        if(nums[i] <= sum)
-        {
-            take = solve(nums, i - 1, sum - nums[i], dp);
-        }
-        return dp[i][sum] = take + notTake;
-    }
-    
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         
