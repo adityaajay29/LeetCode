37dd778e9365a1491e0aceb7e1af023337ff38d5
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-06-10 11:51:52 +0530

    Time: 16 ms (71.42%), Space: 12.1 MB (43.93%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,50 +1,44 @@
 class Solution {
 public:
-    
-    int subsetSum(vector<int> &nums, int n, int sum, int count0)
+    int solve(vector<int> &nums, int i, int sum, vector<vector<int>> &dp)
     {
-        int dp[n+1][sum+1];
-        for(int i=0;i<=n;i++)
+        if(i == 0)
         {
-            for(int j=0;j<=sum;j++)
-            {
-                if(!i)
-                    dp[i][j]=0;
-                if(!j)
-                    dp[i][j]=1;
-            }
+            if(nums[i] == 0 && sum == 0)
+                return 2;
+            
+            if(nums[i] == sum || sum == 0)
+                return 1;
+            
+            return 0;
         }
-        for(int i=1;i<=n;i++)
+        
+        if(dp[i][sum] != -1)
+            return dp[i][sum];
+        
+        int take = 0;
+        int notTake = solve(nums, i - 1, sum, dp);
+        if(nums[i] <= sum)
         {
-            for(int j=1;j<=sum;j++)
-            {
-                if(nums[i-1] <= j && nums[i-1] != 0)
-                    dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
-                else 
-                    dp[i][j] = dp[i-1][j];
-            }
+            take = solve(nums, i - 1, sum - nums[i], dp);
         }
-        return pow(2, count0)*dp[n][sum];
+        return dp[i][sum] = take + notTake;
     }
     
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
-        int range=0;
-//         count of 0 to handle the tc with multiple 0s
-//         ex : [0,0,0,0,0,0,0,0,1]
-//                 1
-//         op : 256, not 1
-        int count0=0;
+        int n = nums.size();
+        
+        int totalSum = 0;
         for(int x : nums)
-        {
-            range+=x;
-            if(x == 0)
-                count0++;
-        }
-//         if the sum is odd, then no combination possible
-        if((range + target)%2 || range < abs(target))
+            totalSum += x;
+        
+        if(totalSum - target < 0 || (totalSum - target) % 2)
             return 0;
-        int sum = (range + target)/2;
-        return subsetSum(nums, n, sum, count0);
+        
+        int sum = (totalSum - target)/2;
+        
+        vector<vector<int>> dp(n, vector<int> (sum + 1, -1));
+        
+        return solve(nums, n-1, sum, dp);
     }
 };
