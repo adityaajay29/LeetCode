ec2955fb5e62a271b56cef207088f21ebd24126a
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-06-10 09:59:52 +0530

    Time: 38 ms (34.51%), Space: 10.9 MB (26.11%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-//         ans will be max of ans and (h-l)
-        int l=0;
-        int h=0;
-        int n=s.size();
-//         ans will be zero for empty string
-        int ans=0;
-//         to keep track of uniqueness
-        unordered_set<char>set;
-        while(l<n && h<n)
+        int l = 0;
+        int h = 0;
+        unordered_set<int> set;
+        int ans = 0;
+        int n = s.size();
+        while(l < n && h < n)
         {
-//             if unique, then add to set and increment the high pointer
-            if(set.find(s[h])==set.end())
-            {  
+            if(set.find(s[h]) == set.end())
+            {
                 set.insert(s[h++]);
-                ans=max(ans,h-l);
+                ans = max(ans, h - l);
             }
-//             else, the character is repeated 
-//             start updating from the left pointer
             else
             {
                 set.erase(s[l++]);
