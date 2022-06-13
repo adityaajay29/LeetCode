301a20ae099f544288f613746cb63562bc316adf
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-06-13 22:15:23 +0530

    Time: 99 ms (50.00%), Space: 6.7 MB (100.00%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool isValid(int maxLimit, vector<int> &cookies, int maxChild)
+    {
+        do
+        {
+            int child = 1;
+            int currLimit = 0;
+            int n = cookies.size();
+            for(int i=0;i<n;i++)
+            {
+                if(currLimit + cookies[i] > maxLimit)
+                {
+                    child++;
+                    currLimit = cookies[i];
+                    
+                    if(currLimit > maxLimit)
+                        return false;
+                }
+                else
+                    currLimit += cookies[i];
+            }
+            if(child <= maxChild)
+                return true;
+        }while(next_permutation(cookies.begin(), cookies.end()));
+        return false;
+    }
+    
+    int distributeCookies(vector<int>& cookies, int k) {
+        sort(cookies.begin(), cookies.end());
+        int n = cookies.size();
+        int l = cookies[n-1] - cookies[0];
+        int h = 0;
+        int ans = 0;
+        for(int x : cookies)
+            h += x;
+        while(l <= h)
+        {
+            int mid = l + (h - l)/2;
+            if(isValid(mid, cookies, k))
+            {
+                ans = mid;
+                h = mid - 1;
+            }
+            else
+                l = mid + 1;
+        }
+        return ans;
+    }
+};
