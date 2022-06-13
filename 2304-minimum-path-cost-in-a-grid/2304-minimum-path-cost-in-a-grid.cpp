6792983cff540e82312ce029dfeb67890e5a6e5c
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-06-13 23:55:29 +0530

    Time: 545 ms (100.00%), Space: 80.3 MB (100.00%) - LeetHub

--- 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
@@ -4,28 +4,30 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         int ans = INT_MAX;
-        vector<vector<int>> dp(m, vector<int> (n));
+        vector<int> prev(n, 0);
         for(int j=0;j<n;j++)
         {
-            dp[m-1][j] = grid[m-1][j];
+            prev[j] = grid[m-1][j];
         }
         
         for(int i=m-2;i>=0;i--)
         {
+            vector<int> curr(n, 0);
             for(int j=n-1;j>=0;j--)
             {
                 int down = INT_MAX;
                 for(int k=0;k<n;k++)
                 {
-                    int curr = moveCost[grid[i][j]][k] + dp[i + 1][k];
+                    int curr = moveCost[grid[i][j]][k] + prev[k];
                     down = min(curr, down);
                 }
-                dp[i][j] = grid[i][j] + down;
+                curr[j] = grid[i][j] + down;
             }
+            prev = curr;
         }
         for(int j=0;j<n;j++)
         {
-            ans = min(ans, dp[0][j]);
+            ans = min(ans, prev[j]);
         }
         return ans;
     }
