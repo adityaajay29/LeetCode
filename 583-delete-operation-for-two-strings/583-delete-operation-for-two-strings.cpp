a36fa8445e4f82ac4569db309c236f371d583a8e
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-06-14 23:30:15 +0530

    Time: 36 ms (38.03%), Space: 11.3 MB (57.55%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -7,27 +7,25 @@ public:
         int n1 = word1.size();
         int n2 = word2.size();
         
-        vector<vector<int>> dp(n1 + 1, vector<int> (n2 + 1));
-        
-        for(int i=0;i<=n1;i++)
-        {
-            dp[i][0] = 0;
-        }
+        vector<int> prev(n2 + 1);
+
         for(int j=0;j<=n2;j++)
         {
-            dp[0][j] = 0;
+            prev[j] = 0;
         }
         
         for(int i=1;i<=n1;i++)
         {
+            vector<int> curr(n2 + 1, 0);
             for(int j=1;j<=n2;j++)
             {
                 if(word1[i - 1] == word2[j - 1])
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    curr[j] = 1 + prev[j - 1];
                 else
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                    curr[j] = max(prev[j], curr[j - 1]);
             }
+            prev = curr;
         }
-        return (( n1 + n2) - (2 * dp[n1][n2]));
+        return (( n1 + n2) - (2 * prev[n2]));
     }
 };
