23bee3fcfb5b14b15239f25c1b7b81035b6f5682
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-06-15 12:24:22 +0530

    Time: 341 ms (11.31%), Space: 73.1 MB (23.43%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    
-    int lcs(string &s1, string &s2, int n, int m)
-    {
+    int longestPalindromeSubseq(string s) {
+        int n = s.size();
+        string s2 = s;
+        reverse(s2.begin(), s2.end());
+        
+        vector<vector<int>> dp(n + 1, vector<int> (n + 1));
+        
         for(int i=0;i<=n;i++)
         {
-            for(int j=0;j<=m;j++)
-            {
-                if(i == 0 || j == 0)
-                    dp[i][j] = 0;
-            }
+            dp[i][0] = 0;
+        }
+        for(int j=0;j<=n;j++)
+        {
+            dp[0][j] = 0;
         }
         for(int i=1;i<=n;i++)
         {
-            for(int j=1;j<=m;j++)
+            for(int j=1;j<=n;j++)
             {
-                if(s1[i-1] == s2[j-1])
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                if(s[i - 1] == s2[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 else
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
-        return dp[n][m];
-    }
-    
-    int longestPalindromeSubseq(string s) {
-        int n=s.size();
-        string revS = s;
-        reverse(s.begin(), s.end());
-        return lcs(s, revS, n, n);
+        return dp[n][n];
     }
 };
