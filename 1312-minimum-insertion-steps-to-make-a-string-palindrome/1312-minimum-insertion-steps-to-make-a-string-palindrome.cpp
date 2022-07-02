7f647bb95226175801d8a4a46fb0774381a51b6d
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-07-02 12:57:21 +0530

    Time: 146 ms (11.47%), Space: 27.2 MB (6.94%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
+    int solve(string &s1, string &s2, int i, int j, vector<vector<int>> &dp)
+    {
+        if(i == 0 || j == 0)
+            return 0;
+        
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        if(s1[i - 1] == s2[j - 1])
+            return dp[i][j] = 1 + solve(s1, s2, i - 1, j - 1, dp);
+        
+        return dp[i][j] = max(solve(s1, s2, i - 1, j, dp), solve(s1, s2, i, j - 1, dp));
+    }
+    
     int minInsertions(string s) {
         int n = s.size();
         string s1 = s;
         reverse(s1.begin(), s1.end());
         
-        vector<int> prev(n + 1, 0);
+        vector<vector<int>> dp(n + 1, vector<int> (n + 1, -1));
         
-        for(int i=1;i<=n;i++)
-        {
-            vector<int> curr(n + 1, 0);
-            for(int j=1;j<=n;j++)
-            {
-                if(s[i - 1] == s1[j - 1])
-                {
-                    curr[j] = 1 + prev[j - 1];
-                }
-                else 
-                    curr[j] = max(prev[j], curr[j - 1]);
-            }
-            prev = curr;
-        }
-        int lcs = prev[n];
+        int lcs = solve(s, s1, n, n, dp);
         
         return n - lcs;
     }
