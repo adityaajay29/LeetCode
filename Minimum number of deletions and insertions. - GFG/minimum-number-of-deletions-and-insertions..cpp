4055ffa6aa13ba7a0e91b294edd49dcdddd29cd2
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-07-02 13:35:37 +0530

    Added solution - LeetHub

--- Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
+++ Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
@@ -5,30 +5,35 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
 	public:
-	int solve(string &s1, string &s2, int i, int j, vector<vector<int>> &dp)
-	{
-	    if(i == 0 || j == 0)
-	    return 0;
-	    
-	    if(dp[i][j] != -1)
-	    return dp[i][j];
-	    
-	    if(s1[i - 1] == s2[j - 1])
-	    return dp[i][j] = 1 + solve(s1, s2, i - 1, j - 1, dp);
-	    
-	    return dp[i][j] = max(solve(s1, s2, i - 1, j, dp), solve(s1, s2, i, j - 1, dp));
-	}
-	
 	int minOperations(string str1, string str2) 
 	{ 
 	    // Your code goes here
 	    int m = str1.size();
 	    int n = str2.size();
 	    
-	    vector<vector<int>> dp(m + 1, vector<int> (n + 1, -1));
+	    vector<vector<int>> dp(m + 1, vector<int> (n + 1, 0));
 	    
-	    int lcs = solve(str1, str2, m, n, dp);
+	    for(int i=0;i<=m;i++)
+	    {
+	        dp[i][0] = 0;
+	    }
+	    for(int j=0;j<=n;j++)
+	    {
+	        dp[0][j] = 0;
+	    }
 	    
+	    for(int i=1;i<=m;i++)
+	    {
+	        for(int j=1;j<=n;j++)
+	        {
+	            if(str1[i - 1] == str2[j - 1])
+	            dp[i][j] = 1 + dp[i - 1][ j - 1];
+	            
+	            else
+	            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+	        }
+	    }
+	    int lcs = dp[m][n];
 	    int del = m - lcs;
 	    int ins = n - lcs;
 	    return del + ins;
