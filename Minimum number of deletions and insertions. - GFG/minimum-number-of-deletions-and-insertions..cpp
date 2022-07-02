f1247cebf7355019497af4bb707926bd8ada00a6
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-07-02 13:38:16 +0530

    Added solution - LeetHub

--- Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
+++ Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
@@ -11,29 +11,22 @@ class Solution{
 	    int m = str1.size();
 	    int n = str2.size();
 	    
-	    vector<vector<int>> dp(m + 1, vector<int> (n + 1, 0));
-	    
-	    for(int i=0;i<=m;i++)
-	    {
-	        dp[i][0] = 0;
-	    }
-	    for(int j=0;j<=n;j++)
-	    {
-	        dp[0][j] = 0;
-	    }
+	    vector<int> prev(n + 1, 0);
 	    
 	    for(int i=1;i<=m;i++)
 	    {
+	        vector<int> curr(n + 1, 0);
 	        for(int j=1;j<=n;j++)
 	        {
 	            if(str1[i - 1] == str2[j - 1])
-	            dp[i][j] = 1 + dp[i - 1][ j - 1];
+	            curr[j] = 1 + prev[ j - 1];
 	            
 	            else
-	            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+	            curr[j] = max(prev[j], curr[j - 1]);
 	        }
+	        prev = curr;
 	    }
-	    int lcs = dp[m][n];
+	    int lcs = prev[n];
 	    int del = m - lcs;
 	    int ins = n - lcs;
 	    return del + ins;
