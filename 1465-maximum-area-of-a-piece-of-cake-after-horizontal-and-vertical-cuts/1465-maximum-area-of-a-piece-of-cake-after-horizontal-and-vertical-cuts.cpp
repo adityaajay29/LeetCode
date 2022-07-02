380426e5bff8d860df1e6017619cac42d247197f
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-07-03 00:42:42 +0530

    Time: 112 ms (37.93%), Space: 34.7 MB (6.04%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -13,28 +13,28 @@ public:
         hor.push_back(horizontalCuts[0]);
         ver.push_back(verticalCuts[0]);
         
-        long long int horMax = hor[0];
-        long long int verMax = ver[0];
+        long long horMax = hor[0];
+        long long verMax = ver[0];
         
         for(int i=1;i<m;i++)
         {
             int cut = horizontalCuts[i] - horizontalCuts[i - 1];
             hor.push_back(cut);
-            horMax = max(horMax, (long long int)hor[i]);
+            horMax = max(horMax, (long long)hor[i]);
         }
         hor.push_back(h - horizontalCuts[m - 1]);
-        horMax = max(horMax, (long long int)hor[m]);
+        horMax = max(horMax, (long long)hor[m]);
         
         for(int i=1;i<n;i++)
         {
             int cut = verticalCuts[i] - verticalCuts[i - 1];
             ver.push_back(cut);
-            verMax = max(verMax, (long long int)ver[i]);
+            verMax = max(verMax, (long long)ver[i]);
         }
         ver.push_back(w - verticalCuts[n - 1]);
-        verMax = max(verMax, (long long int)ver[n]);
+        verMax = max(verMax, (long long)ver[n]);
         
-        long long int cakeMax = (verMax * horMax);
+        long long cakeMax = (verMax * horMax);
         
         return cakeMax % modulo;
     }
