9ce34487df83f78ee459cda91ecc24830becc63b
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-07-04 23:51:51 +0530

    Time: 56 ms (12.87%), Space: 17.8 MB (40.68%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -2,25 +2,24 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> lToR(n, 1);
-        vector<int> rToL(n, 1);
+        vector<int> candies(n, 1);
         
         for(int i=1;i<n;i++)
         {
             if(ratings[i] > ratings[i - 1])
-                lToR[i] = 1 + lToR[i - 1];
+                candies[i] = 1 + candies[i - 1];
         }
         for(int i = n-2;i>=0;i--)
         {
             if(ratings[i] > ratings[i + 1])
-                rToL[i] = 1 + rToL[i + 1];
+                candies[i] = max(candies[i], 1 + candies[i + 1]);
         }
         
         int ans = 0;
         
         for(int i=0;i<n;i++)
         {
-            ans += max(lToR[i], rToL[i]);
+            ans += candies[i];
         }
         return ans;
     }
