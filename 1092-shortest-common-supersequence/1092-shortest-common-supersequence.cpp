c30f3a3152e0279f60b64d7e6fbe4c357e1bc1eb
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-07-04 09:35:29 +0530

    Time: 34 ms (43.14%), Space: 13 MB (36.39%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,69 +1,71 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    
-    void lcs(string &s1, string &s2, int m, int n)
-    {
+    string shortestCommonSupersequence(string str1, string str2) {
+        int m = str1.size();
+        int n = str2.size();
+        
+        vector<vector<int>> dp(m + 1, vector<int> (n + 1, 0));
+        
         for(int i=0;i<=m;i++)
         {
-            for(int j=0;j<=n;j++)
-            {
-                if(i == 0 || j == 0)
-                    dp[i][j] = 0;
-            }
+            dp[i][0] = 0;
         }
+        for(int j=0;j<=n;j++)
+        {
+            dp[0][j] = 0;
+        }
+        
         for(int i=1;i<=m;i++)
         {
-            for(int j=1;j<=n;j++)
+            for(int j = 1;j<=n;j++)
             {
-                if(s1[i-1] == s2[j-1])
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                if(str1[i - 1] == str2[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                
                 else
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
-    }
-    
-    string shortestCommonSupersequence(string str1, string str2) {
-        int m=str1.size();
-        int n=str2.size();
-        lcs(str1, str2, m, n);
-        int i=m;
-        int j=n;
-        string supersequence="";
+        int lcs = dp[m][n];
+        
+        int i = m;
+        int j = n;
+        string ans = "";
         while(i > 0 && j > 0)
         {
-            if(str1[i-1] == str2[j-1])
+            if(str1[i - 1] == str2[j - 1])
             {
-                supersequence.push_back(str1[i-1]);
+                ans.push_back(str1[i - 1]);
                 i--;
                 j--;
             }
             else
             {
-                if(dp[i-1][j] > dp[i][j-1])
+                if(dp[i - 1][j] > dp[i][j- 1])
                 {
-                    supersequence.push_back(str1[i-1]);
+                    ans.push_back(str1[i - 1]);
                     i--;
                 }
                 else
                 {
-                    supersequence.push_back(str2[j-1]);
+                    ans.push_back(str2[j - 1]);
                     j--;
                 }
             }
         }
-        while(i > 0)
+        
+        while(i)
         {
-            supersequence.push_back(str1[i-1]);
+            ans.push_back(str1[i - 1]);
             i--;
         }
-        while(j > 0)
+        while(j)
         {
-            supersequence.push_back(str2[j-1]);
+            ans.push_back(str2[j - 1]);
             j--;
         }
-        reverse(supersequence.begin(), supersequence.end());
-        return supersequence;
+        
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
