223ac08a4e80f36e0c2950ce38356ab1674cc574
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-07-06 12:10:35 +0530

    Time: 97 ms (13.58%), Space: 30 MB (31.06%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -25,8 +25,30 @@ public:
     int numDistinct(string s, string t) {
         int m = s.size();
         int n = t.size();
-        vector<vector<int>> dp(m + 1, vector<int> (n + 1, -1));
+        vector<vector<double>> dp(m + 1, vector<double> (n + 1, 0));
         
-        return solve(s, t, m, n, dp);
+        for(int i=0;i<=m;i++)
+        {
+            dp[i][0] = 1;
+        }
+        
+//         we start from j = 1, as, dp[0][0] should be 1, i.e., when both strings have exhausted
+//         meaning there was a match 
+        for(int j=1;j<=n;j++)
+        {
+            dp[0][j] = 0;
+        }
+        
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(s[i - 1] == t[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return (int)dp[m][n];
     }
 };
