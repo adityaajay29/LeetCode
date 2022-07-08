f5946d454a466faac424421d9df6016b25dd3669
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-07-08 16:45:59 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (91.60%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -11,19 +11,36 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode *root, vector<int> &ans)
-    {
-        if(root == nullptr)
-            return;
-        
-        dfs(root->left, ans);
-        ans.push_back(root->val);
-        dfs(root->right, ans);
-    }
-    
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        dfs(root, ans);
-        return ans;
+        vector<int> inorder;
+        TreeNode *curr = root;
+        while(curr != nullptr)
+        {
+            if(curr->left == nullptr)
+            {
+                inorder.push_back(curr->val);
+                curr = curr->right;
+            }
+            else
+            {
+                TreeNode *prev = curr->left;
+                while(prev->right != nullptr && prev->right != curr)
+                {
+                    prev = prev->right;
+                }
+                if(prev->right == nullptr)
+                {
+                    prev->right = curr;
+                    curr = curr->left;
+                }
+                else
+                {
+                    prev->right = nullptr;
+                    inorder.push_back(curr->val);
+                    curr = curr->right;
+                }
+            }
+        }
+        return inorder;
     }
 };
