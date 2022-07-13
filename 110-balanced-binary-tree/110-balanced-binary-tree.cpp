1c8b320f0c7aaffe6fa253112aae74f2ce16a8eb
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-07-13 21:32:19 +0530

    Time: 20 ms (54.86%), Space: 21.1 MB (25.11%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -18,6 +18,11 @@ public:
         
         int left = height(root->left);
         int right = height(root->right);
+        if(abs(left - right) > 1)
+            return -1;
+        
+        if(left == -1 || right == -1)
+            return -1;
         
         return 1 + max(left, right);
     }
@@ -26,14 +31,6 @@ public:
         if(root == nullptr)
             return true;
         
-        int left = height(root->left);
-        int right = height(root->right);
-        if(abs(left - right) > 1)
-            return false;
-        
-        if(isBalanced(root->left) == false)
-            return false;
-        
-        return isBalanced(root->right);
+        return (height(root) == -1 ? false : true);
     }
 };
