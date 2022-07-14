7bd4f65d65ad9b442b97e32fdc02182371f7468b
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-07-14 15:27:22 +0530

    Time: 27 ms (42.15%), Space: 14.2 MB (57.81%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,45 +9,25 @@
  */
 class Solution {
 public:
-    bool dfs(TreeNode *root, TreeNode *node, vector<TreeNode *> &path)
-    {
-        if(root == nullptr)
-            return false;
-        
-        if(root == node)
-            return true;
-        
-        path.push_back(root);
-        if(dfs(root->left, node, path) || dfs(root->right, node, path))
-            return true;
-        
-        path.pop_back();
-        return false;
-    }
-    
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode *> path1;
-        vector<TreeNode *> path2;
-        dfs(root, p, path1);
-        path1.push_back(p);
+        if(root == nullptr)
+            return nullptr;
         
-        dfs(root, q, path2);
-        path2.push_back(q);
+        if(root == p || root == q)
+            return root;
         
-        int n1 = path1.size();
-        int n2 = path2.size();
-        int i = 0;
-        int j = 0;
-        TreeNode *lca = nullptr;
+        TreeNode *first = lowestCommonAncestor(root->left, p, q);
+        TreeNode *second = lowestCommonAncestor(root->right, p, q);
         
-        while(i < n1 && j < n2)
-        {
-            if(path1[i] != path2[j])
-                break;
-            lca = path1[i];
-            i++;
-            j++;
-        }
-        return lca;
+        if(first && second)
+            return root;
+        
+        if(first && !second)
+            return first;
+        
+        if(!first && second)
+            return second;
+        
+        return nullptr;
     }
 };
