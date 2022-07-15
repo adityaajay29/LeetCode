ae6be79133872498b85f14c2eeca55520b9d25af
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-07-15 12:31:13 +0530

    Time: 117 ms (48.06%), Space: 10.3 MB (64.32%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    int solve(vector<int> &cuts, int i, int j, vector<vector<int>> &dp)
-    {
-        if(i > j)
-            return 0;
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        int mini = INT_MAX;
-        for(int k=i;k<=j;k++)
-        {
-            int part = cuts[j + 1] - cuts[i - 1] + solve(cuts, i, k - 1, dp) + solve(cuts, k + 1, j, dp);
-            mini = min(mini, part);
-        }
-        return dp[i][j] = mini;
-    }
-    
     int minCost(int n, vector<int>& cuts) {
         vector<int> arr = cuts;
         arr.push_back(0);
         arr.push_back(n);
         sort(arr.begin(), arr.end());
         int sz = cuts.size();
-        vector<vector<int>> dp(sz + 1, vector<int> (sz + 1, -1));
-        return solve(arr, 1, sz, dp);
+        vector<vector<int>> dp(sz + 2, vector<int> (sz + 2, 0));
+        for(int i=sz;i>=1;i--)
+        {
+            for(int j=1;j<=sz;j++)
+            {
+                int mini = INT_MAX;
+                for(int k=i;k<=j;k++)
+                {
+                    int part = arr[j + 1] - arr[i - 1] + dp[i][k - 1] + dp[k + 1][j];
+                    mini = min(mini, part);
+                    dp[i][j] = mini;
+                }
+            }
+        }
+        return dp[1][sz];
     }
 };
