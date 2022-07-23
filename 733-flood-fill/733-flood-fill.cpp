8bf80cb38c62d744d08f3abeef395af0ccb52e4a
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-07-23 17:41:44 +0530

    Time: 19 ms (32.33%), Space: 14.5 MB (8.18%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,46 +1,37 @@
 class Solution {
 public:
+    vector<int> dx{1, -1, 0, 0};
+    vector<int> dy{0, 0, -1, 1};
     
-    vector<int>dx{-1,0,0,1};
-    vector<int>dy{0,-1,1,0};
-    int visited[51][51];
-    
-    bool isvalid(int &m,int &n,int x,int y,vector<vector<int>>& image, int sr, int sc)
+    bool isValid(vector<vector<int>>& image, vector<vector<int>> &visited, int i,int j, int sr, int sc, int m, int n)
     {
-        if(x<0 || x>=m || y<0 || y>=n || visited[x][y])
+        if(i < 0 || i >= m || j < 0 || j >= n)
             return false;
-        // if(image[x][y]!=image[sr][sc])
-        //     return false;
+        
+        if(visited[i][j] || image[i][j] != image[sr][sc])
+            return false;
+        
         return true;
     }
     
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        // vector<vector<int>>v;
-        // v=image;
-        int m=image.size();
-        int n=image[0].size();
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
-        visited[sr][sc]=1;
-        int temp=image[sr][sc];
-        image[sr][sc]=-1;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int m = image.size();
+        int n= image[0].size();
+        vector<vector<int>> visited(m, vector<int> (n, 0));
+        queue<pair<int,int>> q;
+        visited[sr][sc] = 1;
+        q.push({sr, sc});
         while(!q.empty())
         {
-            int x=q.front().first;
-            int y=q.front().second;
+            int x = q.front().first;
+            int y = q.front().second;
             q.pop();
             for(int i=0;i<4;i++)
             {
-                int xNext=x+dx[i];
-                int yNext=y+dy[i];
-                if(isvalid(m,n,xNext,yNext,image,sr,sc))
+                if(isValid(image, visited, x + dx[i], y + dy[i], sr, sc, m, n))
                 {
-                    if(image[xNext][yNext]==-1 || image[xNext][yNext]==temp)
-                    {
-                        image[xNext][yNext]=-1;
-                        visited[xNext][yNext]=1;
-                        q.push({xNext,yNext});
-                    }
+                    visited[x + dx[i]][y + dy[i]] = 1;
+                    q.push({x + dx[i], y + dy[i]});
                 }
             }
         }
@@ -48,8 +39,8 @@ public:
         {
             for(int j=0;j<n;j++)
             {
-                if(image[i][j]==-1)
-                    image[i][j]=newColor;
+                if(visited[i][j] == 1)
+                    image[i][j] = color;
             }
         }
         return image;
