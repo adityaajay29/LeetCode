f55c5a9def0422a2da22ac2805c04a9386a410d1
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-07-23 18:42:24 +0530

    Time: 15 ms (54.67%), Space: 14.3 MB (26.16%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -3,46 +3,36 @@ public:
     vector<int> dx{1, -1, 0, 0};
     vector<int> dy{0, 0, -1, 1};
     
-    bool isValid(vector<vector<int>>& image, vector<vector<int>> &visited, int i,int j, int sr, int sc, int m, int n)
+    bool isValid(vector<vector<int>>& image, vector<vector<int>> &visited, int i,int j, int sr, int sc, int m, int n, int same)
     {
         if(i < 0 || i >= m || j < 0 || j >= n)
             return false;
         
-        if(visited[i][j] || image[i][j] != image[sr][sc])
+        if(visited[i][j] || image[i][j] != same)
             return false;
         
         return true;
     }
     
+    void dfs(vector<vector<int>> &image, vector<vector<int>> &visited, int x, int y, int sr, int sc, int m, int n, int same, int color)
+    {
+        visited[x][y] = 1;
+        image[x][y] = color;
+        for(int i=0;i<4;i++)
+        { 
+            if(isValid(image, visited, x + dx[i], y + dy[i], sr, sc, m, n, same))
+            {
+                dfs(image, visited, x + dx[i], y + dy[i], sr, sc, m, n, same, color);
+            }
+        }
+    }
+    
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int m = image.size();
         int n= image[0].size();
+        int same = image[sr][sc];
         vector<vector<int>> visited(m, vector<int> (n, 0));
-        queue<pair<int,int>> q;
-        visited[sr][sc] = 1;
-        q.push({sr, sc});
-        while(!q.empty())
-        {
-            int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
-            for(int i=0;i<4;i++)
-            {
-                if(isValid(image, visited, x + dx[i], y + dy[i], sr, sc, m, n))
-                {
-                    visited[x + dx[i]][y + dy[i]] = 1;
-                    q.push({x + dx[i], y + dy[i]});
-                }
-            }
-        }
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(visited[i][j] == 1)
-                    image[i][j] = color;
-            }
-        }
+        dfs(image, visited, sr, sc, sr, sc, m, n, same, color);
         return image;
     }
 };
