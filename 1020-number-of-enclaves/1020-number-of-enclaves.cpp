246f2b9902b45d14dfa98fd83bfa3cc49df2f330
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-07-29 01:12:34 +0530

    Time: 63 ms (93.31%), Space: 21.6 MB (86.50%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -8,10 +8,7 @@ public:
         if(i < 0 || i >= m || j < 0 || j >= n)
             return false;
         
-        if(grid[i][j] == 0)
-            return false;
-        
-        return true;
+        return (grid[i][j] == 1);
     }
     
     void dfs(vector<vector<int>> &grid, int x, int y, int m, int n)
@@ -27,12 +24,11 @@ public:
     int numEnclaves(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> visited(m, vector<int> (n, 0));
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if((i == 0 || j == 0 || i == m - 1 || j == n - 1) && grid[i][j] == 1)
+                if((i * j == 0 || i == m - 1 || j == n - 1) && grid[i][j] == 1)
                 {
                     dfs(grid, i, j, m, n);
                 }
