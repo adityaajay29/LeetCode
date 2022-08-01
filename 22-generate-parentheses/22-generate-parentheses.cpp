211d86c66299c67f29bb1139e2efe5303babee0b
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-08-01 20:38:37 +0530

    Time: 4 ms (74.20%), Space: 13.7 MB (48.22%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
-    
-    void helper(int open, int close, string temp, vector<string> &ans)
+    void solve(vector<string> &ans, string temp, int open, int close)
     {
-        if(!open && !close)
+        if(open == 0 && close == 0)
         {
             ans.push_back(temp);
             return;
         }
         if(open)
-        {
-            string ans1=temp;
-            ans1.push_back('(');
-            helper(open-1, close, ans1, ans);
-        }
-        if(close>open)
-        {
-            string ans2=temp;
-            ans2.push_back(')');
-            helper(open, close-1, ans2, ans);
-        }
+            solve(ans, temp + '(', open - 1, close);
+        if(close > open)
+            solve(ans, temp + ')', open, close - 1);
     }
     
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        helper(n, n, "", ans);
-        for(auto s:ans)
-            cout<<s<<" ";
+        solve(ans, "", n, n);
         return ans;
     }
 };
