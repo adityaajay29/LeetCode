2f780a374f9e209a66a9c2fbacf1eb0f0ff3955d
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-08-05 15:46:47 +0530

    Time: 2286 ms (8.40%), Space: 5.8 MB (88.61%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    int solve(int i, int n)
-    {
-        if(i == 0 || i == 1)
+    int numTrees(int n) {
+        if(n == 0 || n == 1)
             return 1;
         
-        if(i == 2)
+        if(n == 2)
             return 2;
         
         int cut = 0;
-        for(int k=0;k<n;k++)
+        for(int k = 0; k < n; k++)
         {
-            cut += (solve(k, k) * solve(n - k - 1, n - k - 1)); 
+            cut += (numTrees(k) * numTrees(n - k - 1)); 
         }
         return cut;
-    }
-    
-    int numTrees(int n) {
-        return solve(n, n);
     }
 };
