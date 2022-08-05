240a39a203a35e6d608676113b08e0cf019bc78a
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-08-05 17:09:55 +0530

    Time: 585 ms (52.41%), Space: 8.4 MB (6.67%) - LeetHub

---
+++ 79-word-search/79-word-search.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> dx{1, -1, 0, 0};
+    vector<int> dy{0, 0, 1, -1};
+    
+    bool isValid(int i, int j, int m, int n)
+    {
+        return !(i < 0 || i >= m || j < 0 || j >= n);
+    }
+    
+    bool dfs(vector<vector<char>>& board, int x, int y, int m, int n, string word, int k)
+    {
+        if(k == word.size())
+            return true;
+        
+        char c = board[x][y];
+        board[x][y] = '*';
+        for(int i=0;i<4;i++)
+        {
+            if(isValid(x + dx[i], y + dy[i], m, n))
+            {
+                if(board[x + dx[i]][y + dy[i]] == word[k])
+                {
+                    if(dfs(board, x + dx[i], y + dy[i], m, n, word, k + 1))
+                        return true;
+                }
+            }
+        }
+        board[x][y] = c;
+        return false;
+    }
+    
+    bool exist(vector<vector<char>>& board, string word) {
+        int sz = word.size();
+        int m = board.size();
+        int n = board[0].size();
+        vector<vector<int>> visited(m, vector<int> (n, 0));
+        vector<vector<vector<int>>> dp(m, vector<vector<int>> (n, vector<int> (sz + 1, -1)));
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(board[i][j] == word[0])
+                {
+                    if(dfs(board, i, j, m, n, word, 1))
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+};
