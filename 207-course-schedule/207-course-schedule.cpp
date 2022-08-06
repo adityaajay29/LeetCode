bacc66526d9c96f45ddc41e53f688c05f7a04b8a
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-08-06 09:21:42 +0530

    Time: 46 ms (26.89%), Space: 13.1 MB (95.76%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n = prerequisites.size();
-        vector<int>indegree(numCourses, 0);
         vector<int> list[numCourses];
         for(int i=0;i<n;i++)
         {
-            int u = prerequisites[i][0];
-            int v = prerequisites[i][1];
+            int u = prerequisites[i][1];
+            int v = prerequisites[i][0];
             list[u].push_back(v);
         }
+        vector<int> indegree(numCourses, 0);
         for(int i=0;i<numCourses;i++)
         {
             for(int x : list[i])
@@ -23,21 +23,19 @@ public:
             if(indegree[i] == 0)
                 q.push(i);
         }
-        int ans = 0;
+        int topo = 0;
         while(!q.empty())
         {
             int curr = q.front();
             q.pop();
-            ans++;
+            topo++;
             for(int x : list[curr])
             {
                 indegree[x]--;
                 if(indegree[x] == 0)
-                {
                     q.push(x);
-                }
             }
         }
-        return ans == numCourses;
+        return (topo == numCourses);
     }
 };
