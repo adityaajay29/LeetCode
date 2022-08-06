6661aafc43091dc7ff0b926a94fc39351a667db5
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-08-06 22:06:09 +0530

    Time: 2944 ms (5.03%), Space: 199 MB (6.40%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,49 +1,92 @@
+class MST
+{
+    public : 
+    int u, v, wt;
+    MST(int i, int j, int k)
+    {
+        u = i;
+        v = j;
+        wt = k;
+    }
+};
+
 class Solution {
 public:
+    static bool myComp(MST a, MST b)
+    {
+        return a.wt < b.wt;
+    }
+    
+    int findParent(int a, vector<int> &parent)
+    {
+        if(parent[a] == a)
+            return a;
+         
+        return parent[a] = findParent(parent[a], parent);
+    }
+    
+    void makeUnion(int a, int b, vector<int> &ranked, vector<int> &parent)
+    {
+        a = findParent(a, parent);
+        b = findParent(b, parent);
+        
+        if(ranked[a] > ranked[b])
+            parent[b] = a;
+        
+        else if(ranked[b] > ranked[a])
+            parent[a] = b;
+        
+        else
+        {
+            parent[a] = b;
+            ranked[b]++;
+        }
+    }
+    
+    int kruskals(vector<MST> &list, vector<int> &parent, vector<int> &ranked)
+    {
+        int cost = 0;
+        for(auto x : list)
+        {
+            int u = x.u;
+            int v = x.v;
+            int wt = x.wt;
+            if(findParent(u, parent) != findParent(v, parent))
+            {
+                cost += wt;
+                makeUnion(u, v, ranked, parent);
+            }
+        }
+        return cost;
+    }
+    
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
-        vector<pair<int, int>> list[n];
+        vector<int> parent(n);
         for(int i=0;i<n;i++)
         {
-            vector<int> a = points[i];
-            for(int j=i + 1;j<n;j++)
-            {
-                vector<int> b = points[j];
-                int x1 = a[0];
-                int y1 = a[1];
-                int x2 = b[0];
-                int y2 = b[1];
-                int dist = abs(x1 - x2) + abs(y1 - y2);
-                list[i].push_back({j, dist});
-                list[j].push_back({i, dist});
-            }
+            parent[i] = i;
         }
-        vector<int> mst(n, 0);
-        vector<int> distance(n, 1e9);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({0, 0});
-        distance[0] = 0;
-        while(!pq.empty())
-        {
-            int curr = pq.top().second;
-            pq.pop();
-            mst[curr] = 1;
-            for(auto x : list[curr])
-            {
-                if(!mst[x.first])
-                {
-                    if(distance[x.first] > x.second)
-                    {
-                        distance[x.first] = x.second;
-                        pq.push({distance[x.first], x.first});
-                    }
-                }
-            }
-        }
-        int cost = 0;
-        for(int x : distance)
-            cost += x;
+        vector<int> ranked(n, 0);
+        vector<MST> list;
         
-        return cost;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                vector<int> a = points[i];
+                vector<int> b = points[j];
+                int u = i;
+                int v = j;
+                int x1 = a[0];
+                int x2 = b[0];
+                int y1 = a[1];
+                int y2 = b[1];
+                int wt = abs(x1 - x2) + abs(y1 - y2);
+                list.push_back({u, v, wt});
+            }
+        }
+        sort(list.begin(), list.end(), myComp);
+        return kruskals(list, parent, ranked);
     }
 };
