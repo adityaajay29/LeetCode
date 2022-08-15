b1e94d3a4ea760b3b493a6f7e7be08a5e21844f5
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-08-15 14:22:04 +0530

    Time: 960 ms (44.28%), Space: 237.5 MB (8.04%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.cpp
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -1,58 +1,50 @@
 class Solution {
 public:
-    
-    int merge(vector<int> &nums, int l, int h, int mid)
+    int merge(vector<int> &nums, int l, int mid, int h)
     {
-        int count = 0;
-        int j = mid+1;
+        int res = 0;
+        int j = mid + 1;
         for(int i=l;i<=mid;i++)
         {
-            while(j<=h && nums[i] > 2*(long long)nums[j])
-            {
+            while(j <= h && (long long)nums[i] > (2 * (long long)nums[j]))
                 j++;
-            }
-            count += (j - (mid+1));
+            res += (j - (mid + 1));
         }
         vector<int> temp;
-        int i = l;
-        int k = mid +1;
-        while(i<=mid && k<=h)
+        int i = l, k = mid + 1;
+        while(i <= mid && k <= h)
         {
             if(nums[i] <= nums[k])
-            {
                 temp.push_back(nums[i++]);
-            }
             else
                 temp.push_back(nums[k++]);
         }
         while(i <= mid)
-        {
             temp.push_back(nums[i++]);
-        }
         while(k <= h)
-        {
             temp.push_back(nums[k++]);
-        }
-        for(int i=l;i<=h;i++)
+        for(int i = l;i<=h;i++)
         {
-            nums[i] = temp[i-l];
+            nums[i] = temp[i - l];
         }
-        return count;
+        return res;
     }
     
-    int sort(vector<int> &nums, int l, int h)
+    int mergeSort(vector<int> &nums, int l, int h)
     {
-//         single ele will give 0 pair
         if(l >= h)
             return 0;
-        int mid = l + (h-l)/2;
-        int ans = sort(nums, l, mid);
-        ans += sort(nums, mid+1, h);
-        ans += merge(nums, l, h, mid);
+        
+        int mid = l + (h - l) / 2;
+        int ans = mergeSort(nums, l, mid);
+        ans += mergeSort(nums, mid + 1, h);
+        ans += merge(nums, l, mid, h);
         return ans;
     }
     
     int reversePairs(vector<int>& nums) {
-        return sort(nums, 0, nums.size()-1);
+        int n = nums.size();
+        int l = 0, h = n - 1;
+        return mergeSort(nums, l, h);
     }
 };
