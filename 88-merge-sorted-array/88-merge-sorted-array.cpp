08f77c7ffbc5f5531cf7020bf0fd9bd0cb9fa847
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-08-15 13:59:10 +0530

    Time: 7 ms (28.12%), Space: 9.1 MB (72.00%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -6,16 +6,20 @@ public:
         int k = m + n - 1;
         while(i >= 0 && j >= 0)
         {
-            if(nums1[i] > nums2[j])
+            if(nums1[i] >= nums2[j])
             {
-                nums1[k--] = nums1[i--];
+                swap(nums1[i--], nums1[k--]);
             }
             else
-            {
-                nums1[k--] = nums2[j--];
-            }
+                swap(nums2[j--], nums1[k--]);
+        }
+        while(i >= 0)
+        {
+            swap(nums1[i--], nums1[k--]);
         }
         while(j >= 0)
-            nums1[k--] = nums2[j--];
+        {
+            swap(nums2[j--], nums1[k--]);
+        }
     }
 };
