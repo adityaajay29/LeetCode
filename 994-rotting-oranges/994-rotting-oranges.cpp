3063716cf672c85d125cd245651433091b9a9c35
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-08-17 19:57:30 +0530

    Time: 4 ms (92.51%), Space: 13.2 MB (47.67%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,54 +1,45 @@
 class Solution {
 public:
     vector<int> dx{1, -1, 0, 0};
-    vector<int> dy{0, 0, -1, 1};
+    vector<int> dy{0, 0, 1, -1};
     
-    bool isValid(vector<vector<int>> &grid, int i, int j, int m, int n)
+    bool isValid(int i, int j, int m, int n)
     {
-        if(i < 0 || i >= m || j < 0 || j >= n)
-            return false;
-        
-        if(grid[i][j] != 1)
-            return false;
-        
-        return true;
+        return (i >= 0 && i < m && j >= 0 && j < n);
     }
     
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        
         int oranges = 0;
-        int count = 0;
-        int time = 0;
-
         queue<pair<int, int>> q;
-        
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(grid[i][j] == 2)
-                {
-                    q.push({i, j});
-                }
                 if(grid[i][j] != 0)
                     oranges++;
+                if(grid[i][j] == 2)
+                    q.push({i, j});
             }
         }
-        
+        int time = 0;
+        int rotten = 0;
         while(!q.empty())
         {
-            int size = q.size();
-            count += size;
-            while(size--)
+            int sz = q.size();
+            rotten += sz;
+            while(sz--)
             {
                 int x = q.front().first;
                 int y = q.front().second;
                 q.pop();
                 for(int i=0;i<4;i++)
                 {
-                    if(isValid(grid, x + dx[i], y + dy[i], m, n))
+                    if(!isValid(x + dx[i], y + dy[i], m, n))
+                        continue;
+                    
+                    if(grid[x + dx[i]][y + dy[i]] == 1)
                     {
                         grid[x + dx[i]][y + dy[i]] = 2;
                         q.push({x + dx[i], y + dy[i]});
@@ -58,6 +49,6 @@ public:
             if(!q.empty())
                 time++;
         }
-        return (oranges == count ? time : -1);
+        return (rotten == oranges ? time : -1);
     }
 };
