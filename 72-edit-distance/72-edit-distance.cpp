7ac63945d1c384bf5e3a4b5f23a5781f5b4c4dfd
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-08-28 10:54:52 +0530

    Time: 9 ms (89.27%), Space: 9.4 MB (19.33%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int solve(string &s1, string &s2, int i, int j, vector<vector<int>> &dp)
     {
-        if(i < 0)
-            return j + 1;
+        if(i == 0)
+            return j;
         
-        if(j < 0)
-            return i + 1;
+        if(j == 0)
+            return i;
         
         if(dp[i][j] != -1)
             return dp[i][j];
         
-        if(s1[i] == s2[j])
+        if(s1[i - 1] == s2[j - 1])
             return dp[i][j] = solve(s1, s2, i - 1, j - 1, dp);
         
         int a = solve(s1, s2, i - 1, j, dp);
@@ -24,7 +24,7 @@ public:
     int minDistance(string word1, string word2) {
         int n1 = word1.size();
         int n2 = word2.size();
-        vector<vector<int>> dp(n1, vector<int> (n2, -1));
-        return solve(word1, word2, n1 - 1, n2 - 1, dp);
+        vector<vector<int>> dp(n1 + 1, vector<int> (n2 + 1, -1));
+        return solve(word1, word2, n1, n2, dp);
     }
 };
