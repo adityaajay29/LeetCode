a4a7386d27c101dc83c6fc0232cd93074847a465
Aditya Ajay <90138094+adityaajay29@users.noreply.github.com>
2022-10-19 22:08:42 +0530

    Time: 27 ms (45.47%), Space: 13.4 MB (5.47%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    
+    static bool myComp(pair<string, int> x, pair<string, int> y)
+    {
+        return x.second == y.second ? x.first < y.first : x.second > y.second;
+    }
+    
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        int n = words.size();
+        vector<vector<string>> bucket(n + 1);
+        map<string, int> mp;
+        for(string &s : words)
+        {
+            mp[s]++;
+        }
+        for(auto x : mp)
+        {
+            bucket[x.second].push_back(x.first);
+        }
+        int count = 0;
+        vector<pair<string, int>> ans;
+        for(int i=n;i>=0;i--)
+        {
+            for(string s : bucket[i])
+            {
+                if(count == k)
+                    break;
+                
+                count++;
+                ans.push_back({s, i});
+            }
+        }
+        sort(ans.begin(), ans.end(), myComp);
+        vector<string> res;
+        for(auto x : ans)
+        {
+            res.push_back(x.first);
+        }
+        return res;
+    }
+};
